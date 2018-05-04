2b1d2e771fbbe5e71ba50689981489a5fd5feae3
Ing. Jaime Criollo <Ing. Jaime Criollo@10.5.0.161>
2018-05-03 20:01:43 -0500

    Jeff

--- src/gasolineria2018/Inicio_Administrador.java
+++ src/gasolineria2018/Inicio_Administrador.java
@@ -41,6 +41,8 @@ public class Inicio_Administrador extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(153, 153, 255));
+
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setText("Sistema Gasolinera");
 
